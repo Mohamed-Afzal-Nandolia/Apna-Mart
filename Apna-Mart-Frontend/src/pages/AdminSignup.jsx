292f3445700160a80104b2efe8cf723a41327d0c
Ekansh <efuforofficial@gmail.com>
2024-07-04 23:12:18 +0530

    added signup ui

---
+++ Apna-Mart-Frontend/src/pages/AdminSignup.jsx
@@ -0,0 +1,9 @@
+import { SignupForm } from "../components/SignupForm";
+
+export const AdminSignup = () => {
+  return (
+    <div className="w-full h-screen p-5 flex place-items-center justify-center">
+      <SignupForm />
+    </div>
+  )
+};
