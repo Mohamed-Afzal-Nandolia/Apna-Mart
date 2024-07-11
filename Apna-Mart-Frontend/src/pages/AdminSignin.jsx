d0b3e5a2e0be7fdd0ecf34fc5f189da0af9301e5
Ekansh <efuforofficial@gmail.com>
2024-07-11 21:04:47 +0530

    added signin ui

--- Apna-Mart-Frontend/src/pages/AdminSignin.jsx
+++ Apna-Mart-Frontend/src/pages/AdminSignin.jsx
@@ -1,7 +1,9 @@
-
+import { SigninForm } from "../components/SigninForm";
 
 export const AdminSignin = () => {
   return (
-    <div>AdminSignin</div>
+    <div className="w-full h-screen p-5 flex place-items-center justify-center">
+      <SigninForm />
+    </div>
   )
-}
+};
