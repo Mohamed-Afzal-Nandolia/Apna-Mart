7d518751a175bb6716a210060b812291299504f9
Mohamed-Afzal-Nandolia <afzalcool601+github@gmail.com>
2024-07-20 18:23:52 +0530

    jwt finished

--- backend/src/main/java/net/apnamart/backend/Security/AppConfig.java
+++ backend/src/main/java/net/apnamart/backend/Security/AppConfig.java
@@ -2,6 +2,7 @@ package net.apnamart.backend.Security;
 
 import net.apnamart.backend.entity.Admin;
 import net.apnamart.backend.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -9,30 +10,39 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
 
+    @Autowired
+    private AdminRepository adminRepository;
 
     @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user = User.builder().username("afzal").password(passwordEncoder().encode("ekansh")).roles("ADMIN").build();
-        UserDetails user1 = User.builder().username("cali").password(passwordEncoder().encode("123")).roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(user, user1);
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String a_name) throws UsernameNotFoundException {
+                Admin admin = adminRepository.findByA_name(a_name);
+                if (admin == null) {
+                    throw new UsernameNotFoundException("Admin not found");
+                }
+                return User.withUsername(admin.getA_name())
+                        .password(admin.getA_pass())
+                        .build();
+            }
+        };
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration builder) throws Exception {
-        return builder.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
-
 }
