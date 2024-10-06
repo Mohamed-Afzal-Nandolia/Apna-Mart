926eca8d7669d8d99e9271df27548fb6e6f99461
Ekansh <efuforofficial@gmail.com>
2024-10-06 19:41:22 +0530

    items static changes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,112 +6,82 @@ import { Link } from "react-router-dom";
 export const HomePage = () => {
   const items = [
     {
-        "i_id": 4,
-        "i_name": "Friends",
-        "i_price": 2000,
-        "i_image_path": "D:/GitHub/All Repositories/Apna-Mart/backend/src/main/java/images/friends.jpg",
-        "i_type": "Entertainment",
-        "i_quantity": 200,
-        "i_description": "Good tv series",
-        "i_availability": true
-    },
-    {
-        "i_id": 16,
-        "i_name": "Baygan",
-        "i_price": 100,
-        "i_image_path": null,
-        "i_type": null,
-        "i_quantity": 15,
-        "i_description": "Baygan added for testing purposes",
-        "i_availability": true
-    },
-    {
-        "i_id": 23,
+        "i_id": 36,
         "i_name": "Fresh Pomegranate (Anar)",
         "i_price": 410,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\c6f403eb-d1a1-4f65-a019-c9fd087636e6Fresh Pomegranate (Anar).jpg",
+        "i_image_path": "http://localhost:8085/images/e23f57ba-5a7c-44db-923a-721a9f79ce10.jpg",
         "i_type": "image/jpeg",
         "i_quantity": 4,
         "i_description": "Highly sweet and nutty, pomegranates are packed with multiple health benefits. ",
         "i_availability": true
     },
     {
-        "i_id": 24,
+        "i_id": 37,
         "i_name": "Fresh Watermelon (Tarbooj)",
         "i_price": 160,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\65e41629-9666-4e21-9c5e-52507362dce5Fresh Watermelon.jpg",
+        "i_image_path": "http://localhost:8085/images/5dae743c-5a9d-4e0a-a47e-ca1ab15b00cc.jpg",
         "i_type": "image/jpeg",
         "i_quantity": 5,
         "i_description": "Relish the juicy taste of fresh watermelon. Slice and enjoy every bite or make fresh watermelon juice. You can also use watermelon to make desserts like ice cream. Watermelon seeds can also be dried, roasted and consumed.",
         "i_availability": true
     },
     {
-        "i_id": 25,
-        "i_name": "Apple Royal Gala (Seb)",
-        "i_price": 254,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\a87f4d87-139d-472a-b316-ee7a67269346Apple Royal Gala (Seb).jpg",
-        "i_type": "image/jpeg",
-        "i_quantity": 8,
-        "i_description": "An Apple a day keeps the doctor away. Red Delicious Apples are crunchy juicy and sweet. They are extremely beneficial for your family's health. They are a good source of Fibre and contain valuable anti-oxidants and poly-nutrients.",
-        "i_availability": true
-    },
-    {
-        "i_id": 26,
-        "i_name": "Fresh Banana (Kela)",
-        "i_price": 89,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\f9d225bd-b5ba-43d9-9259-e72816e1cb60Fresh Banana (Kela).jpg",
-        "i_type": "image/jpeg",
-        "i_quantity": 9,
-        "i_description": "Enjoy the taste of banana elaichi or Yelakki, which is consumed across the world for its distinctive flavour and taste.",
-        "i_availability": true
-    },
-    {
-        "i_id": 27,
-        "i_name": "Fresh Papaya (Papita)",
-        "i_price": 88,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\c77d6291-509e-488e-bdd2-02743b5df57eFresh Papaya (Papita).jpg",
-        "i_type": "image/jpeg",
-        "i_quantity": 10,
-        "i_description": "Packed with nutrition, papaya is a healthy addition to your daily quota of fruit consumption. Relish its sweet and fresh flavours!",
-        "i_availability": true
-    },
-    {
-        "i_id": 28,
-        "i_name": "Fresh Pineapple (Ananas)",
-        "i_price": 108,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\ee750889-2a08-46a0-9a30-e4570d774bd5Fresh Pineapple (Ananas).jpg",
-        "i_type": "image/jpeg",
-        "i_quantity": 12,
-        "i_description": "While we work to ensure that the product information is correct, actual product packaging and material may contain more or different information from what is given here.",
-        "i_availability": true
-    },
-    {
-        "i_id": 29,
+        "i_id": 38,
         "i_name": "Avocado Hass Premium Imported",
         "i_price": 130,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\775e5a08-f151-41b5-b28f-8b4042c6683fAvocado Hass Premium Imported.jpg",
+        "i_image_path": "http://localhost:8085/images/20b6127a-d2a8-4a00-a73c-125760d95d45.jpg",
         "i_type": "image/jpeg",
         "i_quantity": 6,
         "i_description": "Avocados are a versatile and nutritious fruit that has gained immense popularity in recent years. Known for their creamy texture and rich flavour, avocados have become a staple ingredient in many households.",
         "i_availability": true
     },
     {
-        "i_id": 30,
+        "i_id": 39,
+        "i_name": "Apple Royal Gala (Seb)",
+        "i_price": 254,
+        "i_image_path": "http://localhost:8085/images/de25c30a-69a5-42b9-8cca-ed1dfb1ebdcd.jpg",
+        "i_type": "image/jpeg",
+        "i_quantity": 8,
+        "i_description": "An Apple a day keeps the doctor away. Red Delicious Apples are crunchy juicy and sweet. They are extremely beneficial for your family's health. They are a good source of Fibre and contain valuable anti-oxidants and poly-nutrients.",
+        "i_availability": true
+    },
+    {
+        "i_id": 40,
+        "i_name": "Fresh Banana (Kela)",
+        "i_price": 89,
+        "i_image_path": "http://localhost:8085/images/76ffe649-42cf-4650-bcb1-76ca5466a158.jpg",
+        "i_type": "image/jpeg",
+        "i_quantity": 9,
+        "i_description": "Enjoy the taste of banana elaichi or Yelakki, which is consumed across the world for its distinctive flavour and taste.",
+        "i_availability": true
+    },
+    {
+        "i_id": 41,
+        "i_name": "Fresh Papaya (Papita)",
+        "i_price": 88,
+        "i_image_path": "http://localhost:8085/images/6bbba081-34c9-4002-aa94-daf2dc8f6e74.jpg",
+        "i_type": "image/jpeg",
+        "i_quantity": 10,
+        "i_description": "Packed with nutrition, papaya is a healthy addition to your daily quota of fruit consumption. Relish its sweet and fresh flavours!",
+        "i_availability": true
+    },
+    {
+        "i_id": 42,
         "i_name": "Fresh Muskmelon (Kharbooja)",
         "i_price": 125,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\0a65d864-45fa-446a-aa88-c1bed0d0be40Fresh Muskmelon (Kharbooja).jpg",
+        "i_image_path": "http://localhost:8085/images/bdc0c8ea-1778-4e5a-8164-563acd4cf504.jpg",
         "i_type": "image/jpeg",
         "i_quantity": 11,
         "i_description": "The most versatile melon around and aptly named honeydew is both sweet and succulent. Its celery-coloured flesh looks like a green-tinged precious stone.",
         "i_availability": true
     },
     {
-        "i_id": 31,
-        "i_name": "Fresh Kiwi",
-        "i_price": 170,
-        "i_image_path": "D:\\GitHub\\All Repositories\\Apna-Mart\\backend\\src\\main\\java\\images\\25994d5b-e10c-44c1-a8e7-451f1e85ce66Fresh Kiwi.jpg",
+        "i_id": 43,
+        "i_name": "Fresh Pineapple (Ananas)",
+        "i_price": 108,
+        "i_image_path": "http://localhost:8085/images/3915b744-d239-4de4-9831-15ecdcd1e990.jpg",
         "i_type": "image/jpeg",
-        "i_quantity": 3,
+        "i_quantity": 12,
         "i_description": "While we work to ensure that the product information is correct, actual product packaging and material may contain more or different information from what is given here.",
         "i_availability": true
     }
@@ -199,4 +169,3 @@ export const HomePage = () => {
     </div>
   );
 };
-
