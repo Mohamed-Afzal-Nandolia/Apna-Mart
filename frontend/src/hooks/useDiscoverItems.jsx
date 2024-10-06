6978207036fd9314c32fcd980ce4fc6e021e43f1
Ekansh <efuforofficial@gmail.com>
2024-10-06 16:50:07 +0530

    added json hard coded data

---
+++ frontend/src/hooks/useDiscoverItems.jsx
@@ -0,0 +1,15 @@
+
+
+// export const useAllItems = () => {
+
+//     const {data, isLoading, error} = useQuery({
+//         queryKey: ["all-items"],
+//         queryFn: () => axios.get("/api/items"),
+//     });
+
+
+//   return useQuery({
+//     queryKey: ["discover-items"],
+//     queryFn: () => axios.get("/api/discover-items"),
+//   });
+// };
